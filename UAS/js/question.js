de291e90e837fbdc443acd4bf534053b4ac7db25
idnyagizka <muhammad.gizka15@gmail.com>
2017-06-19 05:22:16 +0700

    Add files via upload

---
+++ UAS/js/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+		"question": "Hasil penjumlahan dari 15 + 7 = ",
+		"option1": "21",
+		"option2": "22",
+		"option3": "23",
+		"option4": "24",
+		"answer": "2"
+}, {
+		"question": "Hasil pengurangan dari 47 - 13 = ",
+		"option1": "43",
+		"option2": "23",
+		"option3": "34",
+		"option4": "14",
+		"answer": "3"
+}, {
+		"question": "Hasil perkalian dari 12 X 2 = ",
+		"option1": "24",
+		"option2": "6",
+		"option3": "18",
+		"option4": "10",
+		"answer": "1"
+}, {
+		"question": "Nilai tempat puluhan pada bilangan 46 adalah...",
+		"option1": "46",
+		"option2": "6",
+		"option3": "4",
+		"option4": "SALAH SEMUA",
+		"answer": "3"
+}, {
+		"question": "Nilai tempat puluhan pada bilangan 123 adalah...",
+		"option1": "1",
+		"option2": "2",
+		"option3": "3",
+		"option4": "123",
+		"answer": "2"
+}, {
+		"question": "Dikolam ada 75 ekor ikan, diambil sebanyak 15 ekor. Berapa sisa ikan yang ada dikolam?",
+		"option1": "60",
+		"option2": "50",
+		"option3": "40",
+		"option4": "90",
+		"answer": "1"
+}, {
+		"question": "Urutan bilangan dari yang terkecil adalah",
+		"option1": "25,26,27,29,31,30",
+		"option2": "25,28,26,30,27,29,",
+		"option3": "25,30,29,28,27,26",
+		"option4": "25,26,27,28,29,30",
+		"answer": "4"
+}, {
+		"question": "Adik mempunyai 32 kelereng, kakak memberinya 10 kelereng, berapa jumlah kelereng adik seluruhnya ….",
+		"option1": "32",
+		"option2": "22",
+		"option3": "42",
+		"option4": "12",
+		"answer": "3"
+}, {
+		"question": "20 + 18 = 18 + t, Jawaban t adalah...",
+		"option1": "22",
+		"option2": "32",
+		"option3": "20",
+		"option4": "18",
+		"answer": "3"
+}, {
+		"question": "Keranjang merah berisi 20 butir telur, keranjang hijau berisi 40 butir telur dan keranjang kuning berisi 35 butir telur. Yang lebih berat adalah keranjang ….",
+		"option1": "HIJAU",
+		"option2": "MERAH",
+		"option3": "KUNING",
+		"option4": "SAMA BERATNYA",
+		"answer": "1"
+}]
